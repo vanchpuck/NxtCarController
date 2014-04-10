007122d863d5cf4d6c1970f3eb1e464a25302839
vanchpuck <vanchpuck@mail.ru>
2014-04-11 06:35:07 +0900

    Добавлен тормоз. Доработаны повороты.

--- NxtCarController/src/com/jonnygold/nxtcarcontroller/SteeringWheel.java
+++ NxtCarController/src/com/jonnygold/nxtcarcontroller/SteeringWheel.java
@@ -7,8 +7,12 @@ public class SteeringWheel {
 	private static final String TAG = "SteeringWheel";
 	
 	private static final float LEFT_THRESHOLD = -3; //TODO set threshold value
+	private static final float LEFT_MIDDLE = -5;
+	private static final float LEFT_MAX = -7;
 	
 	private static final float RIGHT_THRESHOLD = 3; //TODO set threshold value
+	private static final float RIGHT_MIDDLE = 5;
+	private static final float RIGHT_MAX = 7;
 	
 	
 	private Command previous;
@@ -25,9 +29,21 @@ public class SteeringWheel {
 		if(y > LEFT_THRESHOLD && y < RIGHT_THRESHOLD){
 			command = Command.DIRECT;
 		}	
+		else if(y < LEFT_MAX){
+			command = Command.LEFT_MAX;
+		}
+		else if(y < LEFT_MAX){
+			command = Command.LEFT_MIDDLE;
+		}
 		else if(y < LEFT_THRESHOLD){
 			command = Command.LEFT;
 		}
+		else if(y > RIGHT_MAX){
+			command = Command.RIGHT_MAX;
+		}	
+		else if(y > RIGHT_MIDDLE){
+			command = Command.RIGHT_MIDDLE;
+		}	
 		else if(y > RIGHT_THRESHOLD){
 			command = Command.RIGHT;
 		}	
--- NxtCarController/src/com/jonnygold/nxtcarcontroller/Transmission.java
+++ NxtCarController/src/com/jonnygold/nxtcarcontroller/Transmission.java
@@ -2,22 +2,40 @@ package com.jonnygold.nxtcarcontroller;
 
 public class Transmission implements IsSelector {
 
-//	public enum State{
-//		FORWARD,
-//		BACKWARD
-//	}
+	public enum State{
+		FORWARD (Command.FORWARD),
+		BACKWARD (Command.BACWARD);
+		
+		public final Command command;
+		
+		private State(Command command){
+			this.command = command;
+		}
+	}
 	
 //	private State currentState = State.FORWARD;
 	
 	private Controller controller;
 	
+	private Command lastCommand;
+	
 	public Transmission(Controller controller){
 		this.controller = controller;
+		this.lastCommand = Command.FORWARD;
 	}
 
 	@Override
 	public void shift() {
-		controller.sendCommand(Command.CHANGE_DIRECTION);		
+		switch (lastCommand) {
+		case FORWARD:
+			controller.sendCommand(Command.BACWARD);
+			lastCommand = Command.BACWARD;
+			return;
+		default:
+			controller.sendCommand(Command.FORWARD);
+			lastCommand = Command.FORWARD;
+			return;
+		}
 	}
 	
 }
